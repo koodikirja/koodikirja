2eb6ab51944981ad79d88972e855e1945860ab8d
Juha Paananen <juha.paananen@gmail.com>
2014-02-03 00:03:24 +0200

    smoothly show embedded turtle-roy when in view

--- static/js/book.js
+++ static/js/book.js
@@ -4,12 +4,62 @@ $(function() {
 })
 
 function embedTurtleRoy(elem, options) {
-  var iframe = $('<iframe width=640 height=400 src="http://turtle-roy.herokuapp.com"></iframe>')
+  var width = 640, height = 400
+  var iframe = $('<iframe width=' + width + ' height=' + height + ' src="http://turtle-roy.herokuapp.com"></iframe>')
+  iframe.css({"border": 0})
   elem.append(iframe)
-  elem.css({"position": "relative"})
+  elem.css({"position": "relative",
+            "display": "inline-block",
+            "border": "1px solid black",
+            "width": width + "px",
+            "height": height + "px"})
+  if (true) {
+    var glass = $('<div class="turtle-roy-placeholder">')
+    glass.css({"position": "absolute", 
+               "width": "100%", 
+               "height": "100%", 
+               "z-index": 2, 
+               "top": 0, 
+               "left": 0,
+               "background": "url('/images/roy-placeholder.png') no-repeat"})
+    elem.append(glass)
+
+    whenInView(elem, toggleVisibility)
+
+    function toggleVisibility(visible) {
+      $(".turtle-roy-placeholder").animate({"opacity": visible ? 0 : 1})
+    }
+  }
   if (options.glass) {
     var glass = $('<div class="glass">')
-    glass.css({"position": "absolute", "width": "100%", "height": "100%", "z-index": 1, "top": 0, "left": 0})
+    glass.css({"position": "absolute", 
+               "width": "100%", 
+               "height": "100%", 
+               "z-index": 2, 
+               "top": 0, 
+               "left": 0})
     elem.append(glass)
+  }
+}
+
+function whenInView(elem, f) {
+  var isInView = $(window).asEventStream("scroll").doAction(".preventDefault")
+      .map(function() { return isScrolledIntoView(elem) })
+      .skipDuplicates()
+      .toProperty()
+
+  Bacon.later(3000)
+      .flatMap(isInView)
+      .onValue(function(visible) { f.call(elem, visible) })
+
+  function isScrolledIntoView(elem)
+  {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
 }

521a94d852f60d1fab790f05f8bec35843544f61
Juha Paananen <juha.paananen@gmail.com>
2014-02-15 14:10:25 +0200

    shake the fake turtle when clicking

--- static/js/book.js
+++ static/js/book.js
@@ -5,6 +5,7 @@ $(function() {
       this.toggleClass("grayscale", !visible)
     })
   })
+  $(".turtle-fake").click(function() { $(this).effect("shake", {distance: 5, times: 2})})
   $(".turtle-fake").each(function() {
     var cursor = $("<div>").addClass("cursor")
     $(this).append(cursor)

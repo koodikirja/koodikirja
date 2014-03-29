10c2a18224422cfa0b042f916e478953809f7370
Juha Paananen <juha.paananen@gmail.com>
2014-03-29 09:25:11 +0200

    add basic auth (again)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
+app.use(express.basicAuth('koodikoulu', 'rehtori'));
+
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/output/index.html');
 }).configure(function() {

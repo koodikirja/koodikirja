88a3c2b3036b0db5a5f73347fcd12c4a5c0df0d0
Juha Paananen <juha.paananen@gmail.com>
2014-04-04 22:20:50 +0300

    use basic auth for front page only

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.basicAuth('koodikoulu', 'rehtori'));
+var basicAuth = express.basicAuth('koodikoulu', 'rehtori')
 
-app.get('/', function(request, response) {
+app.get('/', basicAuth, function(request, response) {
     response.sendfile(__dirname + '/output/index.html');
-}).configure(function() {
-    app.use('/', express.static(__dirname + '/output'));
-    app.use('/', express.static(__dirname + '/static'));
-}).listen(port);
+})
+app.use('/', express.static(__dirname + '/output'));
+app.use('/', express.static(__dirname + '/static'));
+
+app.listen(port);

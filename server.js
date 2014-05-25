49eb4241d91972f6ec2788ee7117b9202c96c50b
Juha Paananen <juha.paananen@gmail.com>
2014-05-25 21:32:28 +0300

    wip on adding postgres

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+var Bacon = require('baconjs')
+var pg = require('pg');
+//var pgUrl = "postgres://qlhornyfuuuwqb:1qGtjs7HYWKCwPIYcFzf9xm8Kv@ec2-54-243-42-236.compute-1.amazonaws.com:5432/d6hshjr0tkvfds"
+var pgUrl = "postgres://localhost/koodikirja"
+
+connE = Bacon.fromNodeCallback(pg, "connect", pgUrl)
+connE.onValue(function(client) {
+  console.log("conected to ugUrl")
+})
+connE.errors().log("error")
+
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();

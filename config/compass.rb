08013d1d80f2a17f86a402044e35d79f95365a33
Juha Paananen <juha.paananen@gmail.com>
2014-06-07 23:38:54 +0300

    remove compass.rb

--- config/compass.rb
+++
@@ -1,6 +0,0 @@
-http_path     = '/' 
-project_path  = '../' 
-css_dir       = 'output/stylesheets' 
-sass_dir      = 'content/stylesheets' 
-images_dir    = 'output/images'
-output_style  = :expanded
